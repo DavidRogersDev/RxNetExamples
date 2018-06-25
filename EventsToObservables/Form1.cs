19b8448d508c6f2d4154b6b06a8aceebcf67b9a5
David.Rogers <david.rogers@surgeons.org>
2018-06-25 20:40:02 +0930

    Added the Winforms project example.

---
+++ EventsToObservables/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace EventsToObservables
+{
+    public partial class Form1 : Form
+    {
+        private int a, z;
+        private IDisposable afterHalfwaySubscriber, mirrorCharSubscriber;
+
+        public Form1()
+        {
+            a = Convert.ToInt32('a');
+            z = Convert.ToInt32('z');
+
+            InitializeComponent();
+
+            label2.Text = label1.Text = string.Empty;
+        }
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            var keyPressesObservable = Observable.FromEventPattern<KeyPressEventHandler, KeyPressEventArgs>(
+                    h => textBox1.KeyPress += h,
+                    h => textBox1.KeyPress -= h
+                )
+                .Select(k => k.EventArgs.KeyChar);
+
+            var mirrorCharObservable = keyPressesObservable
+                .Select(k =>
+                    (char)(z - Convert.ToInt32(k) + a)
+                );
+
+            var afterHalfwayObservable = keyPressesObservable
+                .Where(k =>
+                    Convert.ToInt32(k) - a > 26 / 2
+                );
+
+
+            mirrorCharSubscriber = mirrorCharObservable.Subscribe(text =>
+            {
+                label1.Text += text;
+            });
+
+            afterHalfwaySubscriber = afterHalfwayObservable.Subscribe(text =>
+            {
+                label2.Text += text;
+            });
+
+            keyPressesObservable = null; // You can even set the original observable to null!
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            afterHalfwaySubscriber.Dispose();
+            mirrorCharSubscriber.Dispose();
+
+            base.OnFormClosing(e);
+        }
+    }
+}
